80814426f748b5274d128a2b601f7e68354a800a
caio <caio.lima@edu.unifil.br>
2024-01-19 10:22:32 -0300

    feat:Criando getContact, e deleteContact

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Caio',
     email: 'caio@mail.com',
     phone: '1111222',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Pedrinho',
+    email: 'pedrinho@mail.com',
+    phone: '111122233',
+    category_id: v4(),
   },
 ];
 class ContactsRepository {
@@ -14,6 +21,19 @@ class ContactsRepository {
     return new Promise((resolve, reject) => {
       resolve(contacts);
     });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts.filter((contact) => contact.id !== id);
+      resolve();
+    })
   }
 }
 
