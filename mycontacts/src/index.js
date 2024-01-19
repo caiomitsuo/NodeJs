8fb6d7b274dad498c2cc644faa00fc52f8e14774
caio <caio.lima@edu.unifil.br>
2024-01-19 18:03:02 -0300

    feat:Adicionando express.json

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const routes = require('./routes');
 
-const app = express()
-
+const app = express();
+app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () => console.log('Servidor Rodando na porta 3000'));
